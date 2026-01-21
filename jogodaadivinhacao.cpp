86d5a2d2191e1065f62950c514356e9cb399e8c0
Renan <renandiutida@gmail.com>
2026-01-21 18:56:47 -0300

    Adicionando loop para usuario conseguir realizar mais tentativas e contador para registrar quantidade de tentativas

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -9,22 +9,37 @@ int main () {
 
     const int NUMERO_SECRETO = 42;
 
-    int chute;
-    cout << "Qual seu chute? ";
-    cin >> chute;
+    bool nao_acertou = true;
+    int tentativas = 0;
 
-    cout << "O valor do seu chute é: " << chute << endl;
+    while(nao_acertou){
 
-    bool acertou = chute == NUMERO_SECRETO;
-    bool maior = chute > NUMERO_SECRETO;
+        tentativas++;
 
-    if(acertou){
-        cout << "Parabéns! Você acertou o número secreto!" << endl;
-    } 
-    else if (maior) {
-        cout << "Seu chute foi maior que o número secreto!" << endl;
-    } 
-    else {
-        cout << "Seu chute foi menor que o número secreto!" << endl;
+        cout << "Tentativa " << tentativas << endl;
+
+        int chute;
+        cout << "Qual seu chute? ";
+        cin >> chute;
+
+        cout << "O valor do seu chute é: " << chute << endl;
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if(acertou){
+            cout << "Parabéns! Você acertou o número secreto!" << endl;
+            nao_acertou = false;
+        } 
+        else if (maior) {
+            cout << "Seu chute foi maior que o número secreto!" << endl;
+        } 
+        else {
+            cout << "Seu chute foi menor que o número secreto!" << endl;
+        }
     }
+
+    cout << "Fim de jogo!" << endl;
+    cout << "Você acertou o número secreto com " << tentativas << " tentativas!" << endl;
+    
 }
